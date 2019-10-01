4e1c0ce489a6a8d56e8164b45524afa56b1e94c8
DeAdFrOnt <semenukmaks@gmail.com>
2019-10-01 12:05:35 +0300

    feat: return multiply product of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
   // your solution
+  const makeString = ''
+  //number for multiplying
+  const firstNumber=BigInt(first)
+  const secondNumber=BigInt(second)
+  //i wrote all in 1 return but it erroring =/
+  return makeString+firstNumber*secondNumber;
 }
